33b77c4e690f423b0531d7b6430949bc3c0a0941
Boris2514 <borismiashiro@gmail.com>
2023-09-18 21:59:59 -0400

    Updating Doc :V

---
+++ src/main/java/com/example/main/SpringStartHereCh111stApplication.java
@@ -0,0 +1,13 @@
+package com.example.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartHereCh111stApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringStartHereCh111stApplication.class, args);
+  }
+
+}
---
+++ src/main/java/com/example/main/config/ProjectConfig.java
@@ -0,0 +1,9 @@
+package com.example.main.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = "com.example.main.proxy")
+public class ProjectConfig {
+}
